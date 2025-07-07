36fa642cb9012bc97e8d93890fa6592d6b3da3d8
Matthew Sievers <mattersievers@gmail.com>
2025-07-06 21:40:48 -0400

    added DB to secret

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: process.env.SECRET,
+    secret: process.env.DB_SECRET,
     cookie: {
         // timeouts after an hour
         maxAge: 1000*60*60
